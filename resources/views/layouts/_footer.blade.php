b8b8e157dc76e12acef12792f7e677ec1951a5d0
Ciroy <ciroygo@gmail.com>
2019-01-16 03:12:50 +0000

    基础页面结构

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      Pca7是一个具备远景目标的论坛
+    </p>
+
+    <p class="float-right"><a href="11436549@qq.com">联系我</a></p>
+  </div>
+</footer>
