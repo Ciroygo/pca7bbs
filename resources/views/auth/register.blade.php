c48e0a9ffe8c411f31e80b6357443cc400c82212
Ciroy <ciroygo@gmail.com>
2019-01-17 09:31:44 +0000

    注册验证码添加

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -65,14 +65,15 @@
                           <label for="captcha" class="col-md-4 col-form-label text-md-right text-md-right">验证码</label>
 
                           <div class="col-md-6">
-                            <input id="captcha" class="form-control{{ $errors->has('captcha') ? 'is-invalid' : ''}}" name="captcha" required>
+                            <input id="captcha" class="form-control{{ $errors->has('captcha') ? ' is-invalid' : '' }}" name="captcha" required>
 
                             <img class="thumbnail captcha mt-3 mb-2" src="{{ captcha_src('flat') }}" onclick="this.src='/captcha/flat?'+Math.random()" title="重新获取验证码">
 
+
                             @if ($errors->has('captcha'))
-                              <span class="invalid-feedback" role="alert">
-                                <strong>{{ $errors->first('captcha') }}</strong>
-                              </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $errors->first('captcha') }}</strong>
+                                </span>
                             @endif
                           </div>
                         </div>
