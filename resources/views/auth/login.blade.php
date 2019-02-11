f0835e9ef730f66222397b9f0a71179e874c8d23
Ciroy <ciroygo@gmail.com>
2019-02-11 08:42:46 +0000

    忘记密码提交后提醒消息

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -51,8 +51,8 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-offset-md-4">
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
