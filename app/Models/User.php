3a13c5ad2ddf9c59f4bae3fb4379cb8b923d2d9b
Ciroy <ciroygo@gmail.com>
2019-02-12 03:36:32 +0000

    个人资料显示修改

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,14 +10,13 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable implements MustVerifyEmailContract
 {
     use Notifiable, MustVerifyEmailTrait;
-
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'introduction',
     ];
 
     /**
