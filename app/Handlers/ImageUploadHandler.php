83e1990c26f5cef75c7c907cd5e7a27e7e45dcb7
Ciroy <ciroygo@gmail.com>
2019-02-12 05:28:43 +0000

    裁剪头像

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Handlers;
 
+use Intervention\Image\Facades\Image;
+
 /**
  *
  */
@@ -8,7 +10,7 @@ class ImageUploadHandler
 {
     protected $allowed_ext = ['png', 'jpg', 'gif', 'jpeg'];
 
-    public function save($file, $folder, $file_prefix)
+    public function save($file, $folder, $file_prefix, $max_width = false)
     {
         $folder_name = "uploads/images/$folder/" . date("Ym/d", time());
 
@@ -24,8 +26,27 @@ class ImageUploadHandler
 
         $file->move($upload_path, $filename);
 
+        if ($max_width && $extension != 'gif') {
+            $this->reduceSize($upload_path . '/' . $filename, $max_width);
+        }
+
         return [
             'path' => config('app.url') . "/$folder_name/$filename"
         ];
+    }
+
+
+    public function reduceSize($file_path, $max_width)
+    {
+        $image = Image::make($file_path);
+
+        $image->resize($max_width, null, function ($constraint) {
+            $constraint->aspectRatio();
+
+            $constraint->upsize();
+        });
+
+
+        $image->save();
     }
 }
