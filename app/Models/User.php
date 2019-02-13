608658f4b0d6c9927b96dc3bd2481283946517b5
Ciroy <ciroygo@gmail.com>
2019-02-13 03:27:01 +0000

    完成用户话题列表显示 完成第五章节

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Notifications\Notifiable;
+use App\Models\Topic;
 use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable implements MustVerifyEmailContract
 {
@@ -27,4 +28,9 @@ class User extends Authenticatable implements MustVerifyEmailContract
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function topics()
+    {
+        return $this->hasMany(Topic::class);
+    }
 }
